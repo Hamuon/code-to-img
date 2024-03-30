6d760033a34ebabe4caf587040fc0606d7aedc6e
Hamun <hamunjavadzadeghan@gmail.com>
2024-03-30 13:46:13 +0330

    first commit

---
+++ components/Footer.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+function Footer() {
+  return (
+    <div className="flex items-center gap-10 py-16">
+      <div>
+        <Link href="https://github.com/Hamuon">Github</Link>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
